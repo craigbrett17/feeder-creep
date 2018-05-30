110e76f84a8b3eb8a762ef00ba0f5ea08ccdf6c7
Craig Brett <craigbrett17@aol.com>
2018-05-30 21:18:17 +0100

    Abandoned FeedNami and moved to custom parsing
    Moving to rolling my own RSS parsing in response to FeedNami seeming to be
    a paid for API

--- main.js
+++ main.js
@@ -34,49 +34,63 @@ function refreshRssFeeds() {
     const currentList = getFeedUrlsFromLocalStorage();
     if (currentList.length == 0) {
         listElement.innerText = "Unfortunately, you don't seem to have any RSS feeds loaded. Try adding some below and we'll display what they have";
-    } else {
-        listElement.innerText = "";
-        for (let url of currentList) {
-            feednami.load(url)
-                .then(feed => {
-                    console.log(feed);
-                    const outerElement = listElement.appendChild(document.createElement("div"));
-                    const header = document.createElement("h2");
-                    header.innerText = feed.meta.title;
-                    outerElement.appendChild(header);
-                    for (let entry of feed.entries) {
-                        const innerElement = document.createElement("div");
-                        outerElement.appendChild(innerElement);
-                        const anchor = document.createElement("a");
-                        anchor.href = entry.link;
-                        const titleHeader = document.createElement("h3");
-                        titleHeader.innerText = entry.title;
-                        anchor.appendChild(titleHeader);
-                        anchor.setAttribute("target", "_BLANK");
-                        innerElement.appendChild(anchor);
-                        const summary = entry.summary;
-                        const descriptionElement = document.createElement("p");
-                        if (summary && summary.length <= 150) {
-                            descriptionElement.innerHTML = summary;
-                        } else {
-                            if (summary) {
-                                descriptionElement.innerText = "No shortened summary available for this article";
+        return;
+    }
+
+    listElement.innerText = "";
+    for (let url of currentList) {
+        fetch(url)
+            .then(res => {
+                if (res.status < 200 || res.status > 299) {
+                    console.log(`Could not load feed at URL: ${url}. Received response: ${res.status} ${res.statusText}`);
+                    return;
+                }
+
+                res.text()
+                    .then(responseText => {
+                        // load the XML into a dom of its own for easy traversal
+                        var domParser = new DOMParser();
+                        let doc = domParser.parseFromString(responseText, 'text/xml');
+
+                        // set up a div and a heading for this particular feed
+                        const outerElement = listElement.appendChild(document.createElement("div"));
+                        const header = document.createElement("h2");
+                        header.textContent = doc.querySelector('channel>title').textContent;
+                        outerElement.appendChild(header);
+
+                        doc.querySelectorAll('item').forEach((item) => {
+                            const innerElement = document.createElement("div");
+                            outerElement.appendChild(innerElement);
+                            const anchor = document.createElement("a");
+                            anchor.href = item.querySelector('link').textContent;
+                            const titleHeader = document.createElement("h3");
+                            titleHeader.innerText = item.querySelector('title').textContent;
+                            anchor.appendChild(titleHeader);
+                            anchor.setAttribute("target", "_BLANK");
+                            innerElement.appendChild(anchor);
+                            const summary = item.querySelector('description').textContent;
+                            const descriptionElement = document.createElement("p");
+                            if (summary && summary.length <= 150) {
+                                descriptionElement.innerHTML = summary;
                             } else {
-                                descriptionElement.innerText = "No summary field available for this article";
+                                if (summary) {
+                                    descriptionElement.innerText = "No shortened summary available for this article";
+                                } else {
+                                    descriptionElement.innerText = "No summary field available for this article";
+                                }
                             }
-                        }
-                        innerElement.appendChild(descriptionElement);
-                    }
-                })
-        }
+                            innerElement.appendChild(descriptionElement);
+                        });
+                    })
+            })
     }
 }
 
 function refreshFeedsToRemoveList() {
     const removalListElement = document.getElementById('feeds-to-delete-list');
     removalListElement.innerHTML = "";
-    let currentList = getFeedUrlsFromLocalStorage();
-    if (!currentList || currentList.length == 0) {
+    const currentList = getFeedUrlsFromLocalStorage();
+    if (currentList.length == 0) {
         return;
     }
 
